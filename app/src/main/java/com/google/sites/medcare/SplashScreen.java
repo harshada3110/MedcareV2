d95a1f6fa93bcca34f958e317632ce8c7aba3293
Abhishek <abhidhekane@gmail.com>
2019-09-10 18:16:55 +0530

    Splash Screen and Google Sign-In added.

---
+++ app/src/main/java/com/google/sites/medcare/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.google.sites.medcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        final SharedPreferences status = getSharedPreferences("LoginStatus", Context.MODE_PRIVATE);
+
+        new Thread(){
+            public void run(){
+                try {
+                    sleep(3000);
+                    if (status.getBoolean("Value", true)){
+                        startActivity(new Intent(SplashScreen.this,SignIn.class));
+                    }
+                    else {
+                        startActivity(new Intent(SplashScreen.this,Home.class));
+                    }
+                    //startActivity(new Intent(SplashScreen.this,SignIn.class));
+                    SplashScreen.this.finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+}
