ae1551c75cac2320967d854bfbbeb2b91eb11936
Abhishek <abhidhekane@gmail.com>
2019-09-28 08:09:37 +0530

    Pharmacy added.

--- app/src/main/java/com/google/sites/medcare/Pharmacy.java
+++ app/src/main/java/com/google/sites/medcare/Pharmacy.java
@@ -1,14 +1,61 @@
 package com.google.sites.medcare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pharmacy extends AppCompatActivity {
+
+    private RecyclerView PharmacyList;
+    private DatabaseReference mdB2;
+    private PharmacyAdapter adapter2;
+
+    private List<com.google.sites.medcare.PharmacyList> pharmList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pharmacy);
+        PharmacyList = (RecyclerView)findViewById(R.id.mypharmrecycleview);
+        PharmacyList.setLayoutManager(new LinearLayoutManager(this));
+
+        pharmList = new ArrayList<>();
+        adapter2 = new PharmacyAdapter(Pharmacy.this,pharmList);
+        PharmacyList.setAdapter(adapter2);
+        mdB2= FirebaseDatabase.getInstance().getReference("Pharmacy");
+        mdB2.addListenerForSingleValueEvent(valueEventListener);
     }
+
+    ValueEventListener valueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            pharmList.clear();
+            if(dataSnapshot.exists()){
+
+                for(DataSnapshot snapshot:dataSnapshot.getChildren()){
+                    com.google.sites.medcare.PharmacyList pharmacyList = snapshot.getValue(com.google.sites.medcare.PharmacyList.class);
+                    //Log.i("data",""+pharmacy);
+                    pharmList.add(pharmacyList);
+                }
+                adapter2.notifyDataSetChanged();
+            }
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    };
 }
